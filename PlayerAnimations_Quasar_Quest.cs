cf17d197c2a6e2391d6da764479b741b34c7301c
Ahill775 <101592841+Ahill775@users.noreply.github.com>
2022-03-20 22:47:41 -0500

    Add files via upload

---
+++ PlayerAnimations_Quasar_Quest.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Rigidbody2D player;
+    public Sprite[] animationSprites;
+
+    public float animationTime = 1.0f;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private int _animationFrame;
+
+    public float playerSpeed = 5.0f;
+
+    public float jumpForce = 20.0f;
+
+    public Transform feet;
+
+    public LayerMask groundLayers;
+
+    float mx;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += Vector3.left * this.playerSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += Vector3.right * this.playerSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            Jump();
+        }
+    } 
+
+    private void FixedUpdate()
+    {
+        if (player != null)
+        {
+            Vector2 movement = new Vector2(mx * jumpForce, player.velocity.y);
+            player.velocity = movement;
+        }
+         // Flip the character: 
+         Vector3 CharacterScale = transform.localeScale;
+         if (input.GetAxis("Horizontal") < 0) 
+         CharacterScale.x = -10;
+
+    } if (input.GetAxis("Horizontal") > 0) 
+         CharacterScale.x = 10;
+         transform.localeScale = CharacterScale;
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(player.velocity.x, jumpForce);
+        player.velocity = movement;
+    }
+
+    public bool isGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        
+        InvokeRepeating(nameof(AnimateSprite), this.animationTime, this.animationTime);
+    }
+
+    private void AnimateSprite()
+    {
+        _animationFrame++;
+
+        if (_animationFrame >= this.animationSprites.Length)
+        {
+            _animationFrame = 0;
+        }
+
+        _spriteRenderer.sprite = this.animationSprites[_animationFrame];
+    }
+
+    
+}
+
